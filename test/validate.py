da8104ea98e72ca67e2ea35649f4709ba8920e31
ymkz <ymkzly@gmail.com>
2018-09-07 18:54:45 +0900

    wip unresolvable promlem

--- test/validate.py
+++ test/validate.py
@@ -1,6 +1,16 @@
+import sys
 import unittest
 from validate import Validate
+from io import StringIO
 
 class TestValidate(unittest.TestCase):
   def test_validate_addr_spec(self):
-    pass
+    cases = 'abc@example.com\na..bc@example.com\n'
+    dummy_in = StringIO(cases)
+    dummy_out = StringIO()
+    # 実際に実行する
+    validate = Validate()
+    validate.validate_addr_spec(dummy_in, dummy_out)
+    # 結果の比較
+    result = dummy_out.getvalue()
+    self.assertEqual(result, 'ok\nng\n')
--- validate.py
+++ validate.py
@@ -1,3 +1,15 @@
+import re
+
 class Validate:
-  def validate_addr_spec(self):
-    pass
+  def check_addr(self, address):
+    [local, domain] = address.split('@')
+    print(local, domain)
+    p = re.compile("[^a-zA-Z!#\$%&'\*\+\-\/=\?\^_`\\{\|\}~]+")
+    return not(bool(p.match(domain)))
+
+  def validate_addr_spec(self, stream_in, stream_out):
+    results = []
+    for line in stream_in.readlines():
+      results.append('ok' if self.check_addr(line) else 'ng')
+    for result in results:
+      stream_out.write(result + "\n")
