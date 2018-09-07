32de5d99711218c1bc2136063abe9a73eaedde3e
ymkz <ymkzly@gmail.com>
2018-09-07 18:02:37 +0900

    hello

---
+++ test/validate.py
@@ -0,0 +1,6 @@
+import unittest
+from validate import Validate
+
+class TestValidate(unittest.TestCase):
+  def test_validate_addr_spec(self):
+    pass
---
+++ validate.py
@@ -0,0 +1,3 @@
+class Validate:
+  def validate_addr_spec(self):
+    pass
